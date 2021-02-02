cdc743267b1f45bd3049bde01cde449026ca9d64
Rubén Álvarez González <rubenag83@gmail.com>
2021-02-02 12:01:50 +0100

    Se añade la información que va a poder recogerse de la API de SwiftKanban

--- src/Interfaces/IGetKanbanGame.tsx
+++ src/Interfaces/IGetKanbanGame.tsx
@@ -1,5 +1,11 @@
 import IBoard from "./IBoard";
+import ICard from "./ICard";
+import IQueue from "./IQueue";
 
 export default interface IGetKanbanGame{
+  swiftKanbanData: {
     board: IBoard | undefined
+    cards: ICard[],
+    queue: IQueue[]
   }
+}
---
+++ src/Interfaces/IQueue.tsx
@@ -0,0 +1,6 @@
+export default interface IQueue {
+    queueId: number,
+    isParent: string,
+    queueName: string,
+    displayName: string
+}
