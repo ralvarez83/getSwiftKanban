1659eec4cb58331cd86334aecc36745d47836978
Rubén Álvarez González <rubenag83@gmail.com>
2021-02-05 20:18:13 +0100

    Update user.tsx
    Se eliminan warnings por librerías y varianbles no utilizadas

--- src/compontents/user/user.tsx
+++ src/compontents/user/user.tsx
@@ -1,23 +1,16 @@
 import React from 'react';
 import IUser from "../../interfaces/i-user";
-import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
 
-const useStyles = makeStyles({
-  root: {
-    maxWidth: 345,
-  },
-});
 
 interface IProps {
   user: IUser
 }
 
 export default function User (props: IProps){
-  const classes = useStyles();
  
   return (
     <Card>
